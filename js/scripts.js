3531acd5ec6a0918149ae551530ef8e8e6eaf7ea
John <jonnygovish@gmail.com>
2017-08-22 09:54:52 +0300

    Changes in the userinterface displaying the full name

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,11 @@
 //business logic
 function Contact(first, last) {
-  this.firstName = first;
-  this.lastName = last;
-};
+    this.firstName = first;
+    this.lastName = last;
+} 
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
 // user interface logic
 
 $(document).ready(function() {
@@ -12,7 +15,7 @@ $(document).ready(function() {
     var userLastName = $("input#new-lastName").val();
     var newContact = new Contact(userFirstName, userLastName);
 
-    $("ul#contacts").append("<li><span class='contact'> " + newContact.firstName + "</span></li>");
+    $("ul#contacts").append("<li><span class='contact'> " + newContact.fullName() + "</span></li>");
     //shows contact when it is clicked.
     $(".contact").last().click(function() {
       $("#showContacts").show();
